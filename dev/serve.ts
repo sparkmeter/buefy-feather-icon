a9ab7db80b0f4804b847ac6069af86743526a211
Aru Sahni <aru.sahni@sparkmeter.io>
2021-10-17 19:57:19 -0400

    Add and apply prettier.

--- dev/serve.ts
+++ dev/serve.ts
@@ -1,16 +1,19 @@
-import Vue, { VNode } from 'vue';
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
-import Dev from './serve.vue';
-import VueFeather from 'vue-feather';
-import BuefyFeatherIcon from '@/entry.esm';
+import Vue, { VNode } from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import Dev from "./serve.vue";
+import VueFeather from "vue-feather";
+import BuefyFeatherIcon from "@/entry.esm";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueFeather);
 Vue.use(BuefyFeatherIcon);
-Vue.use(Buefy, { defaultIconPack: 'feather-icons', defaultIconComponent: BuefyFeatherIcon });
+Vue.use(Buefy, {
+  defaultIconPack: "feather-icons",
+  defaultIconComponent: BuefyFeatherIcon,
+});
 
 new Vue({
   render: (h): VNode => h(Dev),
-}).$mount('#app');
+}).$mount("#app");
